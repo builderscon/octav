4fc282e21c182e71d42ba0e6faf234421919bd8b
Daisuke Maki <lestrrat+github@gmail.com>
2016-03-04 17:08:44 +0900

    Update schema, and generated files

---
+++ octav/octav_gen_test.go
@@ -0,0 +1,161 @@
+package octav_test
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/builderscon/octav/octav"
+	"github.com/builderscon/octav/octav/client"
+	"github.com/builderscon/octav/octav/validator"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateConference(t *testing.T) {
+	ts := httptest.NewServer(octav.New())
+	defer ts.Close()
+
+	cl := client.New(ts.URL)
+	var in *octav.Conference
+	res, err := cl.CreateConference(in)
+	if !assert.NoError(t, err, "CreateConference should succeed") {
+		return
+	}
+	if !assert.NoError(t, validator.HTTPCreateConferenceResponse.Validate(res), "Validation should succeed") {
+		return
+	}
+}
+
+func TestCreateRoom(t *testing.T) {
+	ts := httptest.NewServer(octav.New())
+	defer ts.Close()
+
+	cl := client.New(ts.URL)
+	var in *octav.Room
+	res, err := cl.CreateRoom(in)
+	if !assert.NoError(t, err, "CreateRoom should succeed") {
+		return
+	}
+	if !assert.NoError(t, validator.HTTPCreateRoomResponse.Validate(res), "Validation should succeed") {
+		return
+	}
+}
+
+func TestCreateSession(t *testing.T) {
+	ts := httptest.NewServer(octav.New())
+	defer ts.Close()
+
+	cl := client.New(ts.URL)
+	var in interface{}
+	res, err := cl.CreateSession(in)
+	if !assert.NoError(t, err, "CreateSession should succeed") {
+		return
+	}
+	if !assert.NoError(t, validator.HTTPCreateSessionResponse.Validate(res), "Validation should succeed") {
+		return
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	ts := httptest.NewServer(octav.New())
+	defer ts.Close()
+
+	cl := client.New(ts.URL)
+	var in interface{}
+	res, err := cl.CreateUser(in)
+	if !assert.NoError(t, err, "CreateUser should succeed") {
+		return
+	}
+	if !assert.NoError(t, validator.HTTPCreateUserResponse.Validate(res), "Validation should succeed") {
+		return
+	}
+}
+
+func TestCreateVenue(t *testing.T) {
+	ts := httptest.NewServer(octav.New())
+	defer ts.Close()
+
+	cl := client.New(ts.URL)
+	var in *octav.Venue
+	res, err := cl.CreateVenue(in)
+	if !assert.NoError(t, err, "CreateVenue should succeed") {
+		return
+	}
+	if !assert.NoError(t, validator.HTTPCreateVenueResponse.Validate(res), "Validation should succeed") {
+		return
+	}
+}
+
+func TestListRooms(t *testing.T) {
+	ts := httptest.NewServer(octav.New())
+	defer ts.Close()
+
+	cl := client.New(ts.URL)
+	var in map[string]interface{}
+	res, err := cl.ListRooms(in)
+	if !assert.NoError(t, err, "ListRooms should succeed") {
+		return
+	}
+	if !assert.NoError(t, validator.HTTPListRoomsResponse.Validate(res), "Validation should succeed") {
+		return
+	}
+}
+
+func TestListSessionsByConference(t *testing.T) {
+	ts := httptest.NewServer(octav.New())
+	defer ts.Close()
+
+	cl := client.New(ts.URL)
+	var in map[string]interface{}
+	res, err := cl.ListSessionsByConference(in)
+	if !assert.NoError(t, err, "ListSessionsByConference should succeed") {
+		return
+	}
+	if !assert.NoError(t, validator.HTTPListSessionsByConferenceResponse.Validate(res), "Validation should succeed") {
+		return
+	}
+}
+
+func TestListVenues(t *testing.T) {
+	ts := httptest.NewServer(octav.New())
+	defer ts.Close()
+
+	cl := client.New(ts.URL)
+	var in map[string]interface{}
+	res, err := cl.ListVenues(in)
+	if !assert.NoError(t, err, "ListVenues should succeed") {
+		return
+	}
+	if !assert.NoError(t, validator.HTTPListVenuesResponse.Validate(res), "Validation should succeed") {
+		return
+	}
+}
+
+func TestLookupSession(t *testing.T) {
+	ts := httptest.NewServer(octav.New())
+	defer ts.Close()
+
+	cl := client.New(ts.URL)
+	var in map[string]interface{}
+	res, err := cl.LookupSession(in)
+	if !assert.NoError(t, err, "LookupSession should succeed") {
+		return
+	}
+	if !assert.NoError(t, validator.HTTPLookupSessionResponse.Validate(res), "Validation should succeed") {
+		return
+	}
+}
+
+func TestLookupVenue(t *testing.T) {
+	ts := httptest.NewServer(octav.New())
+	defer ts.Close()
+
+	cl := client.New(ts.URL)
+	var in map[string]interface{}
+	res, err := cl.LookupVenue(in)
+	if !assert.NoError(t, err, "LookupVenue should succeed") {
+		return
+	}
+	if !assert.NoError(t, validator.HTTPLookupVenueResponse.Validate(res), "Validation should succeed") {
+		return
+	}
+}
--- octav/octav_test.go
+++ octav/octav_test.go
@@ -1,116 +1,15 @@
 package octav_test
 
 import (
-	"net/http/httptest"
-	"testing"
+	"os"
 
-	"github.com/builderscon/octav/octav"
-	"github.com/builderscon/octav/octav/client"
-	"github.com/builderscon/octav/octav/validator"
-	"github.com/stretchr/testify/assert"
+	"github.com/builderscon/octav/octav/db"
 )
 
-func TestCreateConference(t *testing.T) {
-	ts := httptest.NewServer(octav.New())
-	defer ts.Close()
-
-	cl := client.New(ts.URL)
-	var in *octav.Conference
-	res, err := cl.CreateConference(in)
-	if !assert.NoError(t, err, "CreateConference should succeed") {
-		return
-	}
-	if !assert.NoError(t, validator.HTTPCreateConferenceResponse.Validate(res), "Validation should succeed") {
-		return
-	}
-}
-
-func TestCreateRoom(t *testing.T) {
-	ts := httptest.NewServer(octav.New())
-	defer ts.Close()
-
-	cl := client.New(ts.URL)
-	var in *octav.Room
-	res, err := cl.CreateRoom(in)
-	if !assert.NoError(t, err, "CreateRoom should succeed") {
-		return
-	}
-	if !assert.NoError(t, validator.HTTPCreateRoomResponse.Validate(res), "Validation should succeed") {
-		return
-	}
-}
-
-func TestCreateSession(t *testing.T) {
-	ts := httptest.NewServer(octav.New())
-	defer ts.Close()
-
-	cl := client.New(ts.URL)
-	var in interface{}
-	res, err := cl.CreateSession(in)
-	if !assert.NoError(t, err, "CreateSession should succeed") {
-		return
-	}
-	if !assert.NoError(t, validator.HTTPCreateSessionResponse.Validate(res), "Validation should succeed") {
-		return
-	}
-}
-
-func TestCreateUser(t *testing.T) {
-	ts := httptest.NewServer(octav.New())
-	defer ts.Close()
-
-	cl := client.New(ts.URL)
-	var in interface{}
-	res, err := cl.CreateUser(in)
-	if !assert.NoError(t, err, "CreateUser should succeed") {
-		return
-	}
-	if !assert.NoError(t, validator.HTTPCreateUserResponse.Validate(res), "Validation should succeed") {
-		return
-	}
-}
-
-func TestCreateVenue(t *testing.T) {
-	ts := httptest.NewServer(octav.New())
-	defer ts.Close()
-
-	cl := client.New(ts.URL)
-	var in *octav.Venue
-	res, err := cl.CreateVenue(in)
-	if !assert.NoError(t, err, "CreateVenue should succeed") {
-		return
-	}
-	if !assert.NoError(t, validator.HTTPCreateVenueResponse.Validate(res), "Validation should succeed") {
-		return
-	}
-}
-
-func TestListRooms(t *testing.T) {
-	ts := httptest.NewServer(octav.New())
-	defer ts.Close()
-
-	cl := client.New(ts.URL)
-	var in map[string]interface{}
-	res, err := cl.ListRooms(in)
-	if !assert.NoError(t, err, "ListRooms should succeed") {
-		return
-	}
-	if !assert.NoError(t, validator.HTTPListRoomsResponse.Validate(res), "Validation should succeed") {
-		return
-	}
-}
-
-func TestListVenues(t *testing.T) {
-	ts := httptest.NewServer(octav.New())
-	defer ts.Close()
-
-	cl := client.New(ts.URL)
-	var in map[string]interface{}
-	res, err := cl.ListVenues(in)
-	if !assert.NoError(t, err, "ListVenues should succeed") {
-		return
-	}
-	if !assert.NoError(t, validator.HTTPListVenuesResponse.Validate(res), "Validation should succeed") {
-		return
+func init() {
+	if dsn := os.Getenv("OCTAV_TEST_DSN"); dsn != "" {
+		if err := db.Init(dsn); err != nil {
+			panic(err.Error())
+		}
 	}
 }
