7838e26b585ef130ff87d26dcf1b9d7189c463ce
Daisuke Maki <lestrrat+github@gmail.com>
2016-03-11 10:13:30 +0900

    oops

--- octav/travis.sh
+++ octav/travis.sh
@@ -2,19 +2,16 @@
 
 set -e
 
-PDEBUG=1
+if [ -z "$OCTAV_DB_NAME" ]; then
+    OCTAV_DB_NAME=octav
+fi
+
 if [ "$TRAVIS" == "true" ]; then
     echo " + Detected running under Travis CI"
     echo " + Creating database '$OCTAV_DB_NAME'"
     mysql -u root -e "CREATE DATABASE $OCTAV_DB_NAME"
     mysql -u root octav < sql/octav.sql
-
-    # Unset the PDEBUG flag, because 
     go get -t -v -tags debug0 ./...
-fi
-
-if [ -z "$OCTAV_DB_NAME" ]; then
-    OCTAV_DB_NAME=octav
 fi
 
 export OCTAV_TEST_DSN="root:@/$OCTAV_DB_NAME?parseTime=true"
