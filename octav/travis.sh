9c0f8e7e94a3c511715ce74e3bff5e7ab853ea5b
Daisuke Maki <lestrrat+github@gmail.com>
2016-03-11 10:11:48 +0900

    Redirect output to a file

--- octav/travis.sh
+++ octav/travis.sh
@@ -2,18 +2,23 @@
 
 set -e
 
-# tags is required to install optional dependencies
-go get -t -v -tags debug0 ./...
+PDEBUG=1
+if [ "$TRAVIS" == "true" ]; then
+    echo " + Detected running under Travis CI"
+    echo " + Creating database '$OCTAV_DB_NAME'"
+    mysql -u root -e "CREATE DATABASE $OCTAV_DB_NAME"
+    mysql -u root octav < sql/octav.sql
+
+    # Unset the PDEBUG flag, because 
+    go get -t -v -tags debug0 ./...
+fi
 
 if [ -z "$OCTAV_DB_NAME" ]; then
     OCTAV_DB_NAME=octav
 fi
 
-echo " + Creating database '$OCTAV_DB_NAME'"
-mysql -u root -e "CREATE DATABASE $OCTAV_DB_NAME"
-mysql -u root octav < sql/octav.sql
-
 export OCTAV_TEST_DSN="root:@/$OCTAV_DB_NAME?parseTime=true"
 export OCTAV_TRACE_DB=1
+export OCTAV_DEBUG_FILE=debug.out
 
 exec go test -v -tags debug0 
