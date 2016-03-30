6120b97683920b679e62073b84794b5896ccc230
Daisuke Maki <lestrrat+github@gmail.com>
2016-03-30 15:40:00 +0900

    Authenticate user via github
    Note, this is just a placeholder to make it easier for the time
    being. We should not allow users to register via github on
    this domain

--- adminweb/app.psgi
+++ adminweb/app.psgi
@@ -1,7 +1,46 @@
 use strict;
+use Plack::Builder;
 use Octav::AdminWeb;
+use MIME::Base64;
 use Mojo::Server::PSGI;
+use Sereal::Decoder;
+use Sereal::Encoder;
+
+my $web = Octav::AdminWeb->new();
+my $server = Mojo::Server::PSGI->new(app => $web);
+my $app = $server->to_psgi_app;
+
+my $redis = $web->redis();
+my $encoder = Sereal::Encoder->new();
+my $decoder = Sereal::Decoder->new();
+my $store = Plack::Util::inline_object(
+    get => sub {
+        my $h = $redis->command('get', @_);
+        if ($h =~ /^ERR /) {
+            return;
+        }
+        return $h;
+    },
+    set => sub {
+        my $h = $redis->command('set', @_);
+        if ($h =~ /^ERR /) {
+            return
+        }
+        return 1;
+    },
+    remove => sub { $redis->command('del', @_) },
+);
 
 
-my $server = Mojo::Server::PSGI->new(app => Octav::AdminWeb->new());
-return $server->to_psgi_app;
+builder {
+    enable 'Session::Simple',
+        store => $store,
+        serializer => [
+            sub { encode_base64($encoder->encode($_[0])) },
+            sub { $decoder->decode(decode_base64($_[0])) },
+        ],
+        cookie_name => "octav_admin_session",
+
+    ;
+    $app;
+};
---
+++ adminweb/lib/Octav/AdminWeb/Controller/Auth.pm
@@ -0,0 +1,121 @@
+package Octav::AdminWeb::Controller::Auth;
+use Mojo::Base qw(Mojolicious::Controller);
+use JSON ();
+use JSON::Types ();
+use Mojo::UserAgent;
+use URI;
+use UUID::Tiny qw(:std);
+
+sub index {
+    my $self = shift;
+    $self->render(tx => "auth/index");
+}
+
+sub github {
+    my $self = shift;
+
+    my $github_config = $self->config->{github};
+    my $uri = URI->new($github_config->{auth_endpoint});
+    my $redirect_uri = "http://admin.builderscon.io:5000/auth/github_cb";
+    my $state = create_uuid(UUID_RANDOM);
+    my $session = $self->plack_session;
+    $session->{"github_state"} = $state;
+    $session->{"github_state_expires"} = time() + 120;
+    $uri->query_form(
+        client_id    => $github_config->{client_id},
+        redirect_uri => $redirect_uri,
+        scope        => "user",
+        state        => $state,
+    );
+    $self->redirect_to($uri->as_string);
+}
+
+sub github_cb {
+    my $self = shift;
+    my $code = $self->param("code");
+    my $state = $self->param("state");
+    my $github_config = $self->config->{github};
+
+    my $session = $self->plack_session;
+    if (delete $session->{github_state} ne $state) {
+        $self->redirect_to($self->url_for("/auth"));
+        return;
+    }
+
+    if (delete $session->{github_state_expires} < time()) {
+        $self->redirect_to($self->url_for("/auth"));
+        return;
+    }
+
+    my $ua = Mojo::UserAgent->new;
+    my $tx = $ua->post("https://github.com/login/oauth/access_token", 
+        {Accept => "application/json"},
+        form => {
+            client_id     => $github_config->{client_id},
+            client_secret => $github_config->{client_secret},
+            code          => $code,
+        },
+    );
+
+    my $res = $tx->success;
+    if (!$res) {
+        my $err = $tx->error;
+        die "$err->{code} response: $err->{message}" if $err->{code};
+        die "Connection error: $err->{message}";
+    }
+
+    my $oauth_response = JSON::decode_json($res->body);
+    if (my $error = $oauth_response->{error}) {
+        if ($error eq "bad_verification_code") {
+            $self->redirect_to($self->url_for("/auth"));
+            return
+        } else {
+            die "real problem: $error";
+        }
+    }
+    
+    $self->render(text => $oauth_response->{access_token});
+    $tx = $ua->get("https://api.github.com/user", {
+        Authorization => "token $oauth_response->{access_token}",
+    });
+    $res = $tx->success;
+    if (!$res) {
+        die "Failed to get user data from github";
+    }
+
+    my $user = JSON::decode_json($res->body);
+    my $auth_user_id = JSON::Types::string($user->{id});
+    my %data = (
+        auth_via     => "github",
+        auth_user_id => $auth_user_id,
+        avatar_url   => $user->{avatar_url},
+        nickname     => $user->{login},
+    );
+    
+    # name from github *could* be first/last name
+    if (my $name = $user->{name}) {
+        my ($first_name, $last_name) = split(/\s+/, $name, 2);
+        if ($first_name && $last_name) {
+            $data{first_name} = $first_name;
+            $data{last_name} = $last_name;
+        }
+    }
+
+    if (my $email = $user->{email}) {
+        $data{email} = $email;
+    }
+
+    # Race condition ahead...
+    my $client = $self->client();
+    my $registered = $client->lookup_user_by_auth_user_id({auth_via => "github", auth_user_id => $auth_user_id});
+    if (!$registered) {
+        $registered = $client->create_user(\%data);
+    }
+
+    $session->{user} = $registered;
+
+    $self->redirect_to($self->url_for("/user/dashboard"));
+}
+
+1;
+
