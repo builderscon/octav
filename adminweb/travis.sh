b1bd77113ba6c008c1bcf1fa91e60ecba2df431d
Daisuke Maki <lestrrat+github@gmail.com>
2016-03-31 15:43:02 +0900

    installl locally

--- adminweb/travis.sh
+++ adminweb/travis.sh
@@ -7,7 +7,8 @@ perl --version
 echo "====> cpanm version"
 cpanm --version
 echo "====> Installing dependencies..."
-cpanm -v --installdeps --notest .
+cpanm -v --installdeps --notest -Llocal .
 echo "====> Running tests..."
+export PERL5OPT=-Mlib=local/lib/perl5
 perl Build.PL && ./Build && ./Build test
 
