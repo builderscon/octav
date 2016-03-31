18fd14f5d340ae4d7f21c4e5bacad0e545be18a9
Daisuke Maki <lestrrat+github@gmail.com>
2016-03-31 15:36:19 +0900

    Okay, that takes way too long. See if apt perl works

--- adminweb/travis.sh
+++ adminweb/travis.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-curl -L http://install.perlbrew.pl | bash
-source ~/perl5/perlbrew/etc/bashrc
-
-perlbrew install perl-5.22.1
-perlbrew use perl-5.22.1
 perl --version
 cpanm --version
 cpanm --quiet --installdeps --notest .
