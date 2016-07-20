9a31fec1a5ca37cdad53bf3a17e5a9c72679819c
Daisuke Maki <lestrrat+github@gmail.com>
2016-07-20 19:55:12 +0900

    Add conference status
    We want to filter between conferences that we should show and not

--- octav/sql/octav.sql
+++ octav/sql/octav.sql
@@ -70,6 +70,7 @@ CREATE TABLE conferences (
     slug TEXT NOT NULL,
     title TEXT NOT NULL,
     sub_title TEXT,
+    status CHAR(64) CHARACTER SET latin1 NOT NULL default 'private',
     created_by CHAR(64) CHARACTER SET latin1 NOT NULL,
     created_on DATETIME NOT NULL,
     modified_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
