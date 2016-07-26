1d5539aa018e990d76676ec055cc5fc29fb5ff29
Daisuke Maki <lestrrat+github@gmail.com>
2016-07-27 00:15:23 +0900

    fix validation

--- octav/validator/validator.go
+++ octav/validator/validator.go
@@ -875,7 +875,7 @@ func init() {
 		SetConstraintMap(M).
 		SetRoot(
 			jsval.Object().
-				Required("conference_id", "group_name", "logo_url1", "name", "url", "user_id").
+				Required("conference_id", "group_name", "name", "url", "user_id").
 				AdditionalProperties(
 					jsval.EmptyConstraint,
 				).
