cc313e1d130c984d99841638d38d559c060be7ce
Daisuke Maki <lestrrat+github@gmail.com>
2016-08-30 14:37:10 +0900

    Do not automatically generate assets
    This is because assets carry a timestamp, and they pose a
    problem when we check for diffs with Travis CI

--- octav/octav.go
+++ octav/octav.go
@@ -2,6 +2,5 @@
 //go:generate genmodel -d .
 //go:generate gentransport -d .
 //go:generate genoctavctl -s ../spec/v1/api.json -t spec/octavctl.json -o cmd/octavctl/octavctl.go
-//go:generate go-bindata -o assets/assets.go -pkg=assets -ignore=assets.go -prefix=assets assets/...
 
 package octav
