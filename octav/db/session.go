5d684e96ed341c6ac6713a153b779b6f4333f198
Daisuke Maki <lestrrat+github@gmail.com>
2016-10-25 21:48:38 +0900

    remove IS

--- octav/db/session.go
+++ octav/db/session.go
@@ -46,7 +46,7 @@ func (v *SessionList) LoadByConference(tx *Tx, conferenceID, speakerID string, r
 		if where.Len() > 0 {
 			where.WriteString(` AND`)
 		}
-		where.WriteString(` starts_on IS BETWEEN(?, ?)`)
+		where.WriteString(` starts_on BETWEEN(?, ?)`)
 		args = append(args, rangeStart, rangeEnd)
 	}
 
