6a8b131bf907651228660878d9d71849f26776bd
Daisuke Maki <lestrrat+github@gmail.com>
2016-11-16 10:36:57 +0900

    Oops

--- octav/service/cache.go
+++ octav/service/cache.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/builderscon/octav/octav/cache"
+	pdebug "github.com/lestrrat/go-pdebug"
 )
 
 var cacheSvc *cache.Redis
@@ -13,9 +14,14 @@ var cacheOnce sync.Once
 func Cache() *cache.Redis {
 	cacheOnce.Do(func() {
 		addr := os.Getenv("REDIS_ADDR")
-		if addr != "" {
+		if addr == "" {
 			addr = "127.0.0.1:6379"
 		}
+
+		if pdebug.Enabled {
+			pdebug.Printf("Using redis server %s for cache backend", addr)
+		}
+
 		cacheSvc = cache.NewRedis(addr)
 	})
 	return cacheSvc
